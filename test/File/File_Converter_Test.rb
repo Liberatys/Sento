71b7e79d7806866896261b9a5953a912d9f46672
Nick Anthony <ilithium@protonmail.com>
2019-12-03 22:46:55 +0100

    Add plugin-name extraction to the pluginmap as well as a wrapper method to the Sonol object

--- test/File/File_Converter_Test.rb
+++ test/File/File_Converter_Test.rb
@@ -4,15 +4,17 @@ require_relative '../../lib/Sento.rb'
 describe FileConverter do
   it 'parses string into toml object' do
     content = <<-SONOL
-    [testing]
+    [root]
     dasd
-    [good]
+    [bottom]
     adsasd
-    [better]
+    [upper.go.test]
     dasdad
     SONOL
     parser = described_class.new(content)
-    expect(parser.parse_to_sonol.is_a?(Sonol)).to eq(true)
+    sonol_obj = parser.parse_to_sonol
+    expect(sonol_obj.is_a?(Sonol)).to eq(true)
+    expect(sonol_obj.get_plugin_paths).to eq(["root", "bottom", "upper.go.test"])
   end
 
 end
