4676dbb74852164e9c4a164cf8c81ae5cf689c58
Nick Anthony <ilithium@protonmail.com>
2019-12-15 16:18:07 +0100

    Adding support for output and plugin flag

--- bin/Sento.rb
+++ bin/Sento.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative '../lib/Sento.rb'
 
 # TODO: change name for reader method validate file path
@@ -6,27 +7,42 @@ require_relative '../lib/Sento.rb'
 def setup_abyss
   abyss = Abyss.new(Heart.new)
   abyss.add_logger(Logger.new)
-  abyss.add_logger(FileLogger.new('testing.txt'))
+  logger_file_path = 'log.txt'
+  if @reader.get_flag_value_for_key('-o').nil? == false
+    logger_file_path = @reader.get_flag_value_for_key('-o').get_values[0]
+  end
+  abyss.add_logger(FileLogger.new(logger_file_path))
   # ! add plugger
   plugger = Plugger.new('root')
   abyss.add_plugger(plugger)
   abyss
 end
 
+def print_help
+  help_flag = @reader.get_flag_value_for_key('-h')
+  if help_flag.nil? == false
+
+  end
+end
+
 def build_abyss
   abyss = setup_abyss
-  abyss.add_plugin('root.folder_structure', FolderStructurePlugin.new('folder_structure'))
+  # if -p flag is set, load from file
+  if @reader.get_flag_value_for_key('-p').nil? == false
+    abyss = load_by_file(@reader.get_flag_value_for_key('-p').get_values[0], abyss)
+  else
+    abyss.add_plugin('root.folder_structure', FolderStructurePlugin.new('folder_structure'))
+  end
   # ! load plugins
   # ! abyss = load_by_file
   abyss
 end
 
 def load_by_file(_file_string, abyss)
-  file_reader = Reader.new('loader.txt')
+  file_reader = Reader.new(_file_string)
   file_reader.validet_file_path
   loader = PluginLoader.new(file_reader)
   loaded_plugins = loader.load_plugins
-
   loaded_plugins.each do |plug|
     abyss.add_plugin(plug.get_plugin_path, plug.get_plugin)
   end
@@ -35,11 +51,10 @@ def load_by_file(_file_string, abyss)
 end
 
 def read_command_line_args
-  reader = ArgumentReader.new(ARGV)
-  reader.parse_arguments(["-i"])
-  input_file_flag = reader.get_flag_value_for_key("-i")
-  p input_file_flag
-  return input_file_flag.get_values[0]
+  input_file_flag = @reader.get_flag_value_for_key('-i')
+  raise 'no configuration was provided | set -i flag' if input_file_flag.nil?
+
+  input_file_flag.get_values[0]
 end
 
 def read_command_line_arguments
@@ -54,4 +69,6 @@ def read_command_line_arguments
   File.expand_path(file_path)
 end
 
+@reader = ArgumentReader.new(ARGV)
+@reader.parse_arguments(['-i', '-o', '-p', '-h'])
 run_sento(read_command_line_args, build_abyss)
